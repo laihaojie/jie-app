0014f7a335fba6316110c8f8fece788dccc9e5ed
阿杰 <53169820+laihaojie@users.noreply.github.com>
2022-01-14 13:17:06 +0800

    循环list

--- src/views/TaskScreen/TaskScreen.tsx
+++ src/views/TaskScreen/TaskScreen.tsx
@@ -1,10 +1,12 @@
 import { useFocusEffect } from "@react-navigation/native";
 import { NativeStackHeaderProps } from "@react-navigation/native-stack";
 import React, { FC, useCallback } from "react";
-import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import { FlatList, Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Api } from "src/api";
 import { Task } from "src/typings/api";
+import Icon from "react-native-vector-icons/Entypo"
+import { screenWidth } from "src/utils/constants";
 
 export default function TaskScreen() {
   let isMounted = true
@@ -34,10 +36,32 @@ export default function TaskScreen() {
     <SafeAreaView style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }} edges={['top', 'left', 'right']}>
       <ScrollView>
 
+        <FlatList<Task>
+          data={list}
+          style={{ width: screenWidth, }}
+          ListHeaderComponent={
+            <>
+
+              <Text style={styles.title}>添加</Text>
+
+            </>
+          }
+          renderItem={({ item }) =>
+
+            <View style={styles.item}>
+              <Text style={styles.text}>{item.task}</Text>
+              <TouchableOpacity style={styles.right}>
+                <Icon name="dots-three-vertical" style={{ fontSize: 20, }} />
+              </TouchableOpacity>
+
+            </View>
 
 
 
-        <Text style={styles.title}>事件代办页面</Text>
+          }
+          keyExtractor={item => item.id}
+        />
+
 
 
       </ScrollView>
@@ -47,6 +71,26 @@ export default function TaskScreen() {
 
 const styles = StyleSheet.create({
 
+  right: {
+    width: 32,
+    height: 32,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    flex: 1,
+    color: "black",
+    fontSize: 18,
+    fontWeight: "bold",
+    marginHorizontal: 16,
+  },
+  item: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginVertical: 10,
+  },
   title: {
     color: "#333",
     fontSize: 18,
