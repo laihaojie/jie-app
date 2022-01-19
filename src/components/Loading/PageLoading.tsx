5f580b06bb66a90333f2a66bb0da3e8d85a2f02c
阿杰 <53169820+laihaojie@users.noreply.github.com>
2022-01-19 15:11:49 +0800

    添加 Loading 组件

---
+++ src/components/Loading/PageLoading.tsx
@@ -0,0 +1,41 @@
+import React, { Fragment } from "react";
+import { Component } from "react";
+import { ActivityIndicator, View, ViewProps } from "react-native";
+
+interface IProps {
+  show: boolean
+}
+
+export default function PageLoading({ show, children }: IProps & ViewProps) {
+  const [empty, setEmpty] = React.useState(false)
+
+
+
+  React.useEffect(() => {
+    let i;
+    if (show) {
+      i = setInterval(() => {
+        if (show) {
+          setEmpty(true)
+        }
+      }, 3000)
+    }
+    return () => clearInterval(i)
+  }, [])
+
+  return (
+    <>
+      {
+        show ?
+          <View style={{ flex: 1, alignItems: "center", justifyContent: "center", }}>
+            {
+              !empty && <ActivityIndicator color="#55C1F0" size="large" />
+            }
+          </View>
+          :
+          children
+      }
+    </>
+
+  )
+}
